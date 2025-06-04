5a283471578ef4611f156c91868cde4716339009
adutta <>
2025-06-04 12:52:08 +0530

    feat: remove ESI tag from header

--- scripts/aem.js
+++ scripts/aem.js
@@ -653,11 +653,6 @@ function decorateBlocks(main) {
  * @returns {Promise}
  */
 async function loadHeader(header) {
-  // Create the ESI tag
-  const esiTag = document.createElement('esi:include');
-  esiTag.setAttribute('src', '/nav.plain.html');
-  // Append the ESI tag to the header
-  header.appendChild(esiTag);
   const headerBlock = buildBlock('header', '');
   header.append(headerBlock);
   decorateBlock(headerBlock);
